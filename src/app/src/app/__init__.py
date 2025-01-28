523b7b5b5e715ff85b17f18d8ac549bf1c8ec84f
Maksim Kropanov <kropanov@gmail.com>
2025-01-28 18:48:10 +0300

    chore: update code

--- src/app/src/app/__init__.py
+++ src/app/src/app/__init__.py
@@ -1,6 +1,1 @@
-import desktop_ui
-
-
-def main() -> None:
-    desktop_ui.main1()
-    print("Hello from app!")
+from .main import main  # noqa: F401
---
+++ src/app/src/app/main.py
@@ -0,0 +1,10 @@
+import desktop_ui
+
+
+def main():
+    desktop_ui.main1()
+    print("app")
+
+
+if __name__ == "__main__":
+    main()

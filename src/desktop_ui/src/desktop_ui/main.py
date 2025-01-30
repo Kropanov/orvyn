a70d0658c472dfe41d555aba96cf1c3db8f83025
Maksim Kropanov <kropanov@gmail.com>
2025-01-30 19:51:31 +0300

    feat: create layouts and widgets

--- src/desktop_ui/src/desktop_ui/main.py
+++ src/desktop_ui/src/desktop_ui/main.py
@@ -1,9 +1,9 @@
 import kivy
 from kivy.app import App
 from kivy.graphics import Color, Rectangle
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.textinput import TextInput
 
 kivy.require("2.3.1")
 
@@ -12,7 +12,8 @@ class FirstLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(FirstLayout, self).__init__(**kwargs)
 
-        self.size_hint = (0.06, 1)
+        self.size_hint_x = None
+        self.width = 50
 
         with self.canvas.before:
             Color(0.2, 0.6, 0.8, 1)
@@ -21,29 +22,38 @@ class FirstLayout(BoxLayout):
         self.bind(size=self._update_rect, pos=self._update_rect)
 
     def _update_rect(self, instance, value):
-        # Update the rectangle size and position when the layout changes
         self.rect.size = self.size
         self.rect.pos = (0, 0)
 
 
-class SecondLayout(ScatterLayout):
+class SecondLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(SecondLayout, self).__init__(**kwargs)
-        self.size_hint = (0.94, 1)
-        self.cols = 2
+        self.size_hint_x = 1
+        self.size_hint_y = 1
 
         with self.canvas.before:
-            Color(0.1, 0.2, 0.1, 1)
+            Color(0.33, 0.33, 0.33, 1)
             self.rect = Rectangle(pos=(0, 0), size=self.size)
 
         self.bind(size=self._update_rect, pos=self._update_rect)
 
-        self.add_widget(Label(text="Hello world!"))
-
     def _update_rect(self, instance, value):
-        # Update the rectangle size and position when the layout changes
         self.rect.size = instance.size
-        self.rect.pos = (0, 0)
+        self.rect.pos = instance.pos
+
+
+class ThirdLayout(AnchorLayout):
+    def __init__(self, **kwargs):
+        super(ThirdLayout, self).__init__(**kwargs)
+        self.size_hint_x = 1
+        self.size_hint_y = None
+        self.height = 50
+        self.anchor_x = "left"
+        self.anchor_y = "bottom"
+        text_input = TextInput(multiline=True)
+        text_input.background_color = (0.33, 0.33, 0.33, 1)
+        self.add_widget(text_input)
 
 
 class Application(App):
@@ -52,7 +62,11 @@ class Application(App):
     def build(self):
         main_layout = BoxLayout()
         main_layout.add_widget(FirstLayout())
-        main_layout.add_widget(SecondLayout())
+
+        second_layout = SecondLayout()
+        second_layout.add_widget(ThirdLayout())
+
+        main_layout.add_widget(second_layout)
 
         return main_layout
 

47ac71167530d1b1f4583cdcd62cf50d1bd18689
Maksim Kropanov <kropanov@gmail.com>
2025-01-31 23:10:22 +0300

    feat: change the styles of the layouts and elements

--- src/desktop_ui/src/desktop_ui/main.py
+++ src/desktop_ui/src/desktop_ui/main.py
@@ -1,6 +1,6 @@
 import kivy
 from kivy.app import App
-from kivy.graphics import Color, Rectangle
+from kivy.graphics import Color, Line, Rectangle
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
@@ -16,44 +16,51 @@ class FirstLayout(BoxLayout):
         self.width = 50
 
         with self.canvas.before:
-            Color(0.2, 0.6, 0.8, 1)
-            self.rect = Rectangle(pos=(0, 0), size=self.size)
+            # Background color (black)
+            Color(0, 0, 0, 1)
+            self.rect = Rectangle(pos=self.pos, size=self.size)
 
         self.bind(size=self._update_rect, pos=self._update_rect)
 
-    def _update_rect(self, instance, value):
+    def _update_rect(self, *args):
         self.rect.size = self.size
-        self.rect.pos = (0, 0)
+        self.rect.pos = self.pos
 
 
-class SecondLayout(BoxLayout):
+class SecondLayout(AnchorLayout):
     def __init__(self, **kwargs):
         super(SecondLayout, self).__init__(**kwargs)
+        self.anchor_x = "center"
+        self.anchor_y = "bottom"
         self.size_hint_x = 1
         self.size_hint_y = 1
 
-        with self.canvas.before:
-            Color(0.33, 0.33, 0.33, 1)
-            self.rect = Rectangle(pos=(0, 0), size=self.size)
 
-        self.bind(size=self._update_rect, pos=self._update_rect)
-
-    def _update_rect(self, instance, value):
-        self.rect.size = instance.size
-        self.rect.pos = instance.pos
-
-
-class ThirdLayout(AnchorLayout):
+class ThirdLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(ThirdLayout, self).__init__(**kwargs)
-        self.size_hint_x = 1
+        self.size_hint_x = None
         self.size_hint_y = None
         self.height = 50
-        self.anchor_x = "left"
-        self.anchor_y = "bottom"
+        self.width = 650
         text_input = TextInput(multiline=True)
-        text_input.background_color = (0.33, 0.33, 0.33, 1)
+        text_input.background_color = (0, 0, 0, 1)
+        text_input.foreground_color = (1, 1, 1, 1)
+        text_input.hint_text = "Enter text here..."
+
         self.add_widget(text_input)
+        # Add a white top border using canvas
+        with self.canvas.before:
+            Color(1, 1, 1, 1)  # White color
+            self.border_line = Line(
+                points=[self.x, self.top, self.x + self.width, self.top], width=1
+            )
+
+        # Bind size and position updates
+        self.bind(pos=self.update_border, size=self.update_border)
+
+    def update_border(self, *args):
+        self.border_line.points = [self.x, self.top, self.x + self.width, self.top]
 
 
 class Application(App):
